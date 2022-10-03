3fc6e566cc7f619ad3d296041c73dfb7c28d874f
Vyacheslav Khvan <vykhvan@gmail.com>
2022-10-03 20:43:02 +0600

    Update terraform format

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -1,15 +1,15 @@
 terraform {
-   backend "s3" {
-     bucket = "udacity-vykhvan-terraform"
-     key    = "terraform/terraform.tfstate"
-     region = "us-east-2"
-   }
- }
+  backend "s3" {
+    bucket = "udacity-vykhvan-terraform"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
 
- provider "aws" {
-   region = "us-east-2"
+provider "aws" {
+  region = "us-east-2"
 
-   default_tags {
-     tags = local.tags
-   }
- }
+  default_tags {
+    tags = local.tags
+  }
+}
